4ed758d49105c8366f81dcfdb8ff114c0c7854c5
Rubén Javier Alsasua Pérez <r.alsasua@opendeusto.es>
2025-02-25 21:23:58 +0100

    Django

--- Publicador/publicadorApp/models.py
+++ Publicador/publicadorApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField()
+    fecha_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.texto[:50]
--- Subscriptor/subscriptorApp/models.py
+++ Subscriptor/subscriptorApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class MensajeRecibido(models.Model):
+    texto = models.TextField()
+    fecha_recepcion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.texto[:50]
