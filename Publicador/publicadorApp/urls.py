5ba030d2a3c0849046ec20bb4264861415d3657a
Rubén Javier Alsasua Pérez <r.alsasua@opendeusto.es>
2025-03-03 18:35:30 +0100

    Seguir

--- Publicador/publicadorApp/urls.py
+++ Publicador/publicadorApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from publicadorApp import views
 
 urlpatterns = [
-    path('', views.MensajeCreateView.as_view(), name='index'),
+    path('', views.index, name='index'),
+    path('envio/', views.MensajeCreateView.as_view(), name='crear'),
 ]
--- Publicador/publicadorApp/views.py
+++ Publicador/publicadorApp/views.py
@@ -6,7 +6,11 @@ import os
 import ssl
 
 
+def index(request):
+    return render(request, 'index.html')
+
 def enviar_por_mqtt(mensaje):
+    print("Enviando mensaje")
     client = mqtt.Client()
     try:
         client.tls_set(
@@ -29,12 +33,13 @@ class MensajeCreateView(View):
     def get(self, request):
         formulario = MensajeForm()
         print("el get")
-        return render(request, "index.html", {"formulario": formulario})
+        return render(request, "envio.html", {"formulario": formulario})
 
     def post(self, request):
         formulario = MensajeForm(data=request.POST)
         if formulario.is_valid():
             mensaje = formulario.cleaned_data['texto']
             enviar_por_mqtt(mensaje)
+            formulario.save()
             return redirect("index")
-        return render(request, "index.html", {"formulario": formulario})
+        return render(request, "envio.html", {"formulario": formulario})
